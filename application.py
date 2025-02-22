6a0624d9e5a6316e28cdce9e741196fbb37213a8
Ahmad Rosid Komarudin <arrosid@Ahmads-MacBook-Air.local>
2025-02-23 05:50:09 +0700

    dev2

--- application.py
+++ application.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 import os
 from dotenv import load_dotenv
 
-
 # Load environment variables from .env file
 load_dotenv()
 
@@ -14,9 +13,12 @@ name = os.getenv("NAME")
 if not name:
     raise ValueError("Environment variable 'NAME' not found. Use .env file to set environment variable!")
 
+# Get the port from environment variable or default to 5000
+port = int(os.getenv("PORT", 4321))
+
 @flask_app.route('/')
 def index():
     return render_template('index.html', name=name)
 
 if __name__ == '__main__':
-    flask_app.run(debug=True)
+    flask_app.run(debug=True, port=port)
