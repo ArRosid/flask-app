0900d2d028e04d0a77e747c6a7d1dfad9e29763e
Ahmad Rosid Komarudin <arrosid@Ahmads-MacBook-Air.local>
2025-02-23 09:32:59 +0700

    app

--- application.py
+++ application.py
@@ -7,16 +7,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Initialize Flask app
-flask_app = Flask(__name__)
+app = Flask(__name__)
 
 # Get the name from environment variable
 name = os.getenv("NAME")
 if not name:
     raise ValueError("Environment variable 'NAME' not found. Use .env file to set environment variable!")
 
-@flask_app.route('/')
+@app.route('/')
 def index():
     return render_template('index.html', name=name)
 
 if __name__ == '__main__':
-    flask_app.run(debug=True)
+    app.run(debug=True)
